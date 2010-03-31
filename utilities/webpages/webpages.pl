0a4235896c6515cf079335603f56592eaa17b036
Troy Will <troydwill@gmail.com>
2010-03-31 15:36:29 -0700

    modified:   utilities/webpages/webpages.pl 	modified:   utilities/webpages/webpages.sh

--- utilities/webpages/webpages.pl
+++ utilities/webpages/webpages.pl
@@ -9,7 +9,7 @@ open( HTML, '>webpages.html') || die "Unable to open file for writing";
 my $DATABASE = '/var/lib/stowball/gng-repository.db';
 my $dbh = DBI->connect("dbi:SQLite:$DATABASE", "", "", {RaiseError => 1, AutoCommit => 1});
 
-my $query = qq(SELECT packages.id, packages.short_name, webpages.homepage_url FROM webpages INNER JOIN packages ON webpages.id = packages.id);
+my $query = qq(SELECT packages.id, packages.short_name, webpages.homepage_url FROM webpages INNER JOIN packages ON webpages.id = packages.id ORDER BY packages.short_name);
 my $sth = $dbh->prepare($query);
 $sth->execute();
 
@@ -17,7 +17,7 @@ print HTML "<TABLE>\n";
 my @values =();
 while (my ($id, $short_name, $homepage_url ) = $sth->fetchrow_array) {
     print "$id, $short_name, $homepage_url\n";
-    print HTML "<TR><TD><a href='$homepage_url'>$homepage_url</a></TD></TR>\n";
+    print HTML "<TR><TD>$short_name</TD><TD><a href='$homepage_url'>$homepage_url</a></TD></TR>\n";
 }
 print HTML "</TABLE>\n";
 
--- utilities/webpages/webpages.sh
+++ utilities/webpages/webpages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DATABASE='/var/lib/stowball/gng-repository.db'
-sqlite3 ${DATABASE} 'SELECT id, homepage_url FROM webpages order by id'
+# sqlite3 ${DATABASE} 'SELECT id, homepage_url, wikipedia_url FROM webpages order by id'
+sqlite3 ${DATABASE} 'SELECT packages.id, packages.short_name, webpages.homepage_url FROM webpages INNER JOIN packages ON webpages.id = packages.id'
+
 
 
