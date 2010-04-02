c33cd0c6e8fb55437fda58ffb51e594377e7e90b
Troy Will <troydwill@gmail.com>
2010-04-01 19:48:24 -0700

    modified:   ../populate.sh

--- data/populate.sh
+++ data/populate.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+set -o errexit
 EDITOR='emacs'
+DIRECTORY=$1
+cd ${DIRECTORY}
 ${EDITOR} packages.csv latest_tarball.csv
 ${EDITOR} packages.csv webpages.csv
 ${EDITOR} packages.csv package_metas.csv
 git add *
 git commit
 git push origin master
+cd -
 cd ..
 ./stowball-write-repository-data
