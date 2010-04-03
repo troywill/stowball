e954525a45420fe58b72d055560ee632366e5aff
Troy Will <troydwill@gmail.com>
2010-04-02 23:37:31 -0700

    modified:   net-bootstrap.pl

--- utilities/net-bootstrap.pl
+++ utilities/net-bootstrap.pl
@@ -1,12 +1,22 @@
 #!/usr/bin/env perl
 my $rsync_module = 'troywill.com::stowballs';
 my $download_dir = '/tmp';
+my $stow_dir = '/stow';
 
-my @stowballs = ( 'Text-CSV-1.17', 'ConfigReader-Simple-1.28', 'DBD-SQLite-1.29', 'rsync-3.0.7' );
+my @stow_packages = ( 'Text-CSV-1.17', 'ConfigReader-Simple-1.28', 'DBD-SQLite-1.29', 'rsync-3.0.7' );
 
-foreach my $stowball ( @stowballs ) {
-    $stowball = $stowball . '.stow.tar.bz2';
-    my $shell_command = "rsync --archive --progress $rsync_module/$stowball $download_dir";
+# Retrieve
+foreach my $stow_package ( @stow_packages ) {
+    my $stowball = $stow_package . '.stow.tar.bz2';
+    my $shell_command = "rsync --archive $rsync_module/$stowball $download_dir";
     print "\n==> $stowball\n";
+    print `$shell_command`;
+}
+
+# Unpack and stow
+foreach my $stow_package ( @stow_packages ) {
+    my $stowball = $stow_package . '.stow.tar.bz2';
+    chdir $stow_dir;
+    my $shell_command = "tar --extract --file $download_dir/$stowball";
     print `$shell_command`;
 }
