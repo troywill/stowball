4f50def96ac1df4283c5b2e158831706250ffd87
Troy Will <troydwill@gmail.com>
2010-04-02 23:22:02 -0700

    modified:   net-bootstrap.pl

--- utilities/net-bootstrap.pl
+++ utilities/net-bootstrap.pl
@@ -1,13 +1,12 @@
 #!/usr/bin/env perl
-
 my $rsync_module = 'troywill.com::stowballs';
+my $download_dir = '/tmp';
 
 my @stowballs = ( 'Text-CSV-1.17', 'ConfigReader-Simple-1.28', 'DBD-SQLite-1.29', 'rsync-3.0.7' );
 
 foreach my $stowball ( @stowballs ) {
     $stowball = $stowball . '.stow.tar.bz2';
-    my $shell_command = "rsync -av $rsync_module/$stowball";
-    print "$stowball\n";
-    print $shell_command, "\n";
+    my $shell_command = "rsync --archive --progress $rsync_module/$stowball $download_dir";
+    print "\n==> $stowball\n";
     print `$shell_command`;
 }
