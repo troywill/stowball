9817a8099372b1be097c612ec521bdec2834ee68
Troy Will <troydwill@gmail.com>
2010-04-09 22:44:11 -0700

    new file:   update_last_checked_date

--- utilities/last_checked.sql
+++ utilities/last_checked.sql
@@ -1,6 +1,6 @@
 -- USEAGE: sqlite3 /var/lib/stowball/2010_spring.db < last_checked.sql
 
-SELECT packages.date_checked, packages.short_name, packages.latest_version, webpages.homepage_url, webpages.wikipedia
+SELECT packages.date_checked, packages.short_name, packages.latest_version, webpages.homepage_url, webpages.wikipedia_url
 FROM packages
      INNER JOIN webpages
      	   ON webpages.id = packages.id
